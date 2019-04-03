0a508c5dab04c518ad38d0220bcf1e8005944b98
Elena Shen <35210702+tikishen@users.noreply.github.com>
2019-04-02 22:58:26 -0400

    Add files via upload

---
+++ comment-app-advanced/comment-app-advanced/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyCommentApp from './MyCommentApp';
+import './index.css';
+
+ReactDOM.render(
+  <MyCommentApp />,
+  document.getElementById('root')
+);
