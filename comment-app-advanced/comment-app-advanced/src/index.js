8413990efd22bb511c070ef968395db2788b9b93
TikiShen <tqshen7@gmail.com>
2019-04-20 15:26:23 -0400

    Update

--- comment-app-advanced/comment-app-advanced/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MyCommentApp from './MyCommentApp';
-import './index.css';
-
-ReactDOM.render(
-  <MyCommentApp />,
-  document.getElementById('root')
-);
